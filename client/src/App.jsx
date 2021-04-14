e17d1654feb4ec350ed7eeac29c5f08014f8952a
kidgray <micah.allauca@gmail.com>
2021-04-13 22:26:16 -0400

    Created HomePage component and styles

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { HomePage } from './components/homepage/homepage.component.jsx';
+
+export const App = () => (
+    <div>
+        <HomePage />
+    </div>
+)
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 
+import { App } from './App.jsx';
 
-const template = <p>Hello from React!</p>;
-
-ReactDOM.render(template, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
