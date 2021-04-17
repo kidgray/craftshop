842bac0e5100638146ae4a8f621c4633719436c8
kidgray <micah.allauca@gmail.com>
2021-04-17 01:43:09 -0400

    MenuItem styling

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 export const MenuItem = (props) => (
     <div 
         className={`${props.size} menu-item` }
-        style={{
-            backgroundImage: `url(${props.imageUrl})`
-        }}
     >
+        <div 
+            className='background-image' 
+            style={{
+                backgroundImage: `url(${props.imageUrl})`
+            }} 
+        />
         <div className='content'>
             <h1 className='title'>{ props.title }</h1>
             <span>BROWSE INVENTORY</span>
